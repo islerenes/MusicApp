f0b4ace1ce4ed69a14398b1bb6098c1571974e59
enesisler1905 <58097246+enesisler1905@users.noreply.github.com>
2023-07-30 15:25:13 +0300

    NewsAPI Added

--- MusicApp/AppDelegate.swift
+++ MusicApp/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-    // yorum satiri enes
+    // yorum satiri iki
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
